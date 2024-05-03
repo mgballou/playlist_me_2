040504b0d1bf47d3ae83330d0c8957648bd6dc45
mgballou <matthewbuiltthat@gmail.com>
2024-05-03 11:58:00 -0400

    teal header

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <div className="flex min-h-12 w-full flex-row justify-between bg-slate-700 p-4">
+        <div className="flex min-h-12 w-full flex-row justify-between bg-teal-700 p-4">
             <p>Playlist.me</p>
             <nav>Links go here</nav>
         </div>
