938180b4f0dbb22147cfb6982e4f881bf092524a
mgballou <matthewbuiltthat@gmail.com>
2024-05-07 23:38:32 -0400

    span bg effects

--- components/home/CTAButton.tsx
+++ components/home/CTAButton.tsx
@@ -21,15 +21,16 @@ export default function CTAButton() {
                     mass: 0.1,
                 },
             }}
-            
             size="xl"
-            className="radial-gradient relative hover:bg-emerald-200/25"
+            className="radial-gradient group relative drop-shadow-md hover:bg-emerald-200/25"
             variant={'ghost'}
         >
             <span className="linear-mask relative block h-full w-full">
                 start
             </span>
             <span className="linear-overlay absolute inset-0 block rounded-md p-px" />
+            <span className="absolute h-1/5 w-2/3 rounded-xl blur-xl group-hover:-bottom-4 group-hover:right-12 group-hover:bg-emerald-400" />
+            <span className="absolute h-1/5 w-1/3 rounded-xl blur-xl group-hover:-top-6 group-hover:left-20 group-hover:bg-emerald-200" />
         </Button>
     )
 }
