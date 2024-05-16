6d9a2304bc01a95d280340c6455f7eccfbf223fb
mgballou <matthewbuiltthat@gmail.com>
2024-05-16 00:08:15 -0400

    rough track cards

--- app/generator/page.tsx
+++ app/generator/page.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { Button } from '@/components/ui/button'
 import { getTrackData, getAPIToken } from '../actions'
 import { useEffect, useState } from 'react'
@@ -14,35 +12,23 @@ interface SpotifyTrack {
 }
 
 
-export default function Generator() {
+export default async function Generator() {
     const tracks = ['1xzBco0xcoJEDXktl7Jxrr']
-    const [results, setResults] = useState<null | SpotifyTrack[]>()
-
-    useEffect(() => {
-
-        async function getData(){
-            const data = await getTrackData(tracks)
-            setResults(data)
-        }
-
-        getData()
-
-    }, [])
-
-    
+    const results: SpotifyTrack[] = await getTrackData(tracks)
+   
 
     return (
         <main className="flex min-h-screen flex-col justify-evenly p-24">
             Welcome 2 generator
-            <Button onClick={async () => await getTrackData(tracks)}>
+            {/* <Button onClick={async () => await getTrackData(tracks)}>
                 Dummy results
             </Button>
             <Button onClick={async () => await getAPIToken()}>
                 Dummy Token
-            </Button>
+            </Button> */}
             
 
-            <div className='flex flex-row flex-wrap gap-2 bg-slate-600 inset-4 w-full min-h-60 justify-center'>
+            <div className='flex flex-row flex-wrap gap-2 bg-slate-600 inset-4 w-full min-h-60 justify-center p-4'>
 
                 {results?.map((track, idx) => {
                     return (<div key={track.spotifyId}>
--- components/shared/TrackCard.tsx
+++ components/shared/TrackCard.tsx
@@ -24,24 +24,33 @@ interface Props {
 export default function TrackCard({ track }: Props) {
     return (
         <>
-            {/* <Card className="w-48">
-            <CardContent className='w-full p-0'>
+        <div className="flex h-full flex-col">
+        <Card className=" w-80 flex-grow flex flex-col gap-2 ">
+            <div className='flex flex-row border-b-4 border-slate-700'>
+            <CardContent className='w-1/3 p-0'>
                 <Image
                     src={track.albumArtwork}
-                    width={'128'}
-                    height={'128'}
+                    width={'112'}
+                    height={'112'}
                     alt={`Album artwork for ${track.tName} by ${track.tArtist}`}
-                    className='mx-auto'
+                    className=''
                 ></Image>
             </CardContent>
-            <CardHeader>
-                <CardTitle>{track.tArtist}</CardTitle>
+            <CardHeader className=' text-left w-2/3 space-y-0.5 p-2 border border-red-500'>
+                <CardTitle className='text-sm'>{track.tArtist}</CardTitle>
                 <CardDescription>{track.tName}</CardDescription>
             </CardHeader>
+            </div>
+            <CardFooter className='h-14'>
+
+            </CardFooter>
 
             
-        </Card> */}
-            <div className="flex h-full flex-col">
+        </Card>
+            
+        </div>
+            
+            {/* <div className="flex h-full flex-col">
                 <div className="bg-card flex w-40 flex-grow flex-col items-center gap-2">
                     <Image
                         src={track.albumArtwork}
@@ -59,7 +68,7 @@ export default function TrackCard({ track }: Props) {
                         </ul>
                     </div>
                 </div>
-            </div>
+            </div> */}
         </>
     )
 }
