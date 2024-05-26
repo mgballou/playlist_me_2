f613840a9429fd00267964bc4bf742c80afcf5d8
mgballou <matthewbuiltthat@gmail.com>
2024-05-26 11:41:11 -0400

    basic link styling

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,7 +1,15 @@
+import Link from 'next/link'
+
 export default function Header() {
     return (
         <div className="flex min-h-12 w-full flex-row justify-between bg-gradient-to-br from-transparent from-55% via-indigo-700 via-60% to-emerald-400/70 to-90% p-4">
-            <p>Playlist.me</p>
+            <Link
+                href={'/'}
+                className="px-2 text-lg font-semibold decoration-indigo-400 hover:bg-slate-100/25 hover:text-indigo-400 hover:underline focus:bg-slate-100/25 focus:text-indigo-400 focus:underline"
+            >
+                Playlist.me
+            </Link>
+
             <nav>Links go here</nav>
         </div>
     )
