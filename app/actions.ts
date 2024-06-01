4cd8230da5b5245ccaa9a305fe11a2b06eae8a7d
mgballou <matthewbuiltthat@gmail.com>
2024-06-01 15:55:33 -0400

    refactor actions.ts for better function naming, revalidation behavior, and add new getTrackData function

--- app/actions.ts
+++ app/actions.ts
@@ -1,25 +1,23 @@
 'use server'
-import { cookies } from 'next/headers'
-import { SpotifyToken } from '@/lib/types'
-import path from 'path'
-import fs from 'fs'
+
+import { SpotifyTrack } from '@/lib/types'
 
 export async function getAPIToken() {
     return (
         await fetch('http://localhost:3000/api/spotify/auth', {
-            next: { revalidate: 3000 },
+            next: { revalidate: 3600 },
         })
     ).json()
 }
 
-export async function getTrackData(tracks: string[]) {
+export async function getRecommendationsData(trackIds: string[]) {
     // this entire thing can just be fetched in a server component instead
 
     const token = await getAPIToken()
 
     const url = 'https://api.spotify.com/v1/recommendations'
 
-    const selectionsString = tracks.join(',')
+    const selectionsString = trackIds.join(',')
 
     // TODO: remove any
     const params = new URLSearchParams({
@@ -36,8 +34,36 @@ export async function getTrackData(tracks: string[]) {
     }
     try {
         const response = await fetch(url + '?' + params.toString(), options)
+        if (!response.ok) {
+            // Handle non-OK responses, e.g., rate limiting (status code 429)
+            if (response.status === 429) {
+                const retryAfter = response.headers.get('Retry-After')
+
+                if (retryAfter) {
+                    // Retry-After can be in seconds or a date string
+                    const retryAfterSeconds = parseInt(retryAfter, 10)
+
+                    if (!isNaN(retryAfterSeconds)) {
+                        console.log(
+                            `Rate limited. Retry after ${retryAfterSeconds} seconds.`
+                        )
+                    } else {
+                        console.log(`Rate limited. Retry after ${retryAfter}.`)
+                    }
+                } else {
+                    console.log('Rate limited. No Retry-After header present.')
+                }
+
+                throw new Error('Too many requests')
+            }
+            // Handle other types of errors
+            throw new Error(`HTTP error! status: ${response.status}`)
+        }
         const responseData = await response.json()
+
         // remove any
+
+        // this section could be a second function that just takes the ids and grabs the rest of the data for better error handling
 
         const endpoint = 'https://api.spotify.com/v1/audio-features'
 
@@ -50,6 +76,7 @@ export async function getTrackData(tracks: string[]) {
             endpoint + '?' + secondParams.toString(),
             options
         )
+        // handle error
         const otherFeaturesData = await otherFeatures.json()
 
         const results = responseData.tracks?.map((track: any, idx: number) => {
@@ -67,6 +94,44 @@ export async function getTrackData(tracks: string[]) {
                 energy: otherFeaturesData.audio_features[idx].energy,
                 instrumentalness:
                     otherFeaturesData.audio_features[idx].instrumentalness,
+            }
+        })
+
+        return results
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export async function getTrackData(trackIds: string[]) {
+    const token = await getAPIToken()
+    const selectionsString = trackIds.join(',')
+    const url = 'https://api.spotify.com/v1/tracks/'
+
+    const params = new URLSearchParams({
+        ids: selectionsString,
+        market: 'US',
+    })
+
+    const options = {
+        method: 'GET',
+        headers: { Authorization: `Bearer ${token}` },
+    }
+
+    try {
+        const response = await fetch(url + '?' + params.toString(), options)
+
+        // handle error
+
+        const responseData = await response.json()
+
+        const results = responseData.tracks?.map((track: any, idx: number) => {
+            return {
+                title: track.name,
+                artist: track.artists[0].name,
+                link: track.external_urls.spotify,
+                spotifyId: track.id,
+                artwork: track.album.images[1].url,
             }
         })
 
