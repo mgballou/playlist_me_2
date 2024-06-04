92f190bdc318b15e56cdf532c4d4e1b8a28307c1
mgballou <matthewbuiltthat@gmail.com>
2024-06-03 21:46:34 -0400

    additional ui in sidebar

--- components/generator/TracksSidebar.tsx
+++ components/generator/TracksSidebar.tsx
@@ -1,19 +1,55 @@
 'use client'
 
+// React hooks
 import { useState, useEffect } from 'react'
 
-import { getTrackData } from '@/app/actions'
-import TrackCard from '@/components/shared/TrackCard'
+// External libraries
+import clsx from 'clsx'
 
+// Next.js navigation hooks
+import { usePathname, useRouter } from 'next/navigation'
+
+// Application actions
+import { getTrackData } from '@/app/actions'
+
+// Application components
+import TrackCard from '@/components/shared/TrackCard'
+import { Button } from '../ui/button'
+
+// Context providers
 import { useTracksStore } from '@/context/providers/tracks-store-provider'
+
+// Application types
 import { SpotifyTrack } from '@/lib/types'
 
 export default function TracksSidebar() {
-    const { tracks, addTrack, removeTrack } = useTracksStore((state) => state)
+    const pathname = usePathname()
+    const router = useRouter()
+
+    console.log(pathname)
+
+    const { tracks, addTrack, removeTrack, clearTracks } = useTracksStore(
+        (state) => state
+    )
 
     const [selections, setSelections] = useState<SpotifyTrack[]>([])
-    
-    // function clearTracks(){} ?? in store
+
+    function handleNextPage() {
+        switch (pathname) {
+            case 'generator':
+                router.push('/generator/search')
+                break
+            case '/generator/search':
+                router.push('/generator/adjustments')
+                break
+            case '/generator/adjustments':
+                router.push('/generator/results')
+                // Add logic for CASE_THREE here
+                break
+            default:
+                break
+        }
+    }
 
     async function getSelections() {
         if (tracks.length > 0) {
@@ -39,16 +75,35 @@ export default function TracksSidebar() {
     }, [tracks])
 
     return (
-        <div className={(tracks.length > 0 ? 'w-1/3' : 'w-0') + 'bg-slate-400'}>
-            {selections?.map((selection) => {
-        return (
-            <TrackCard
-                key={'sidebar' + selection.spotifyId}
-                variant={'selection'}
-                track={selection}
-            />
-        )
-    })}
-        </div>
+        <>
+            {tracks.length > 0 && (
+                <div
+                    className={
+                        'flex w-1/3 flex-col justify-start bg-slate-400 p-4'
+                    }
+                >
+                    <div className="flex h-12 flex-row justify-center">
+                        <Button onClick={() => clearTracks()}>
+                            Clear Selections
+                        </Button>
+                        <Button onClick={() => handleNextPage()}>
+                            Next Step
+                        </Button>
+                    </div>
+
+                    <div className="flex flex-col gap-2 overflow-scroll">
+                        {selections?.map((selection) => {
+                            return (
+                                <TrackCard
+                                    key={'sidebar' + selection.spotifyId}
+                                    variant={'selection'}
+                                    track={selection}
+                                />
+                            )
+                        })}
+                    </div>
+                </div>
+            )}
+        </>
     )
 }
