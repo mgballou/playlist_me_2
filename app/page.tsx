ae3e58604bb8531024adcef51b63a60759c8d6df
mgballou <matthewbuiltthat@gmail.com>
2024-06-09 22:52:31 -0400

    provider around demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import CTA from '@/components/home/CTA'
 import Demo from '@/components/home/Demo'
+import { TracksStoreProvider } from '@/context/providers/tracks-store-provider'
 
 export default async function Home() {
     return (
-        <section className="flex flex-row justify-evenly p-24">
+        <section className="flex flex-row justify-evenly px-16 py-20">
             <CTA />
-            <Demo />
+            <TracksStoreProvider>
+                <Demo />
+            </TracksStoreProvider>
         </section>
     )
 }
