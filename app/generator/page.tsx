3464e015261102ee6279ce13e2cf85246762ee4a
mgballou <matthewbuiltthat@gmail.com>
2024-06-13 20:29:53 -0400

    change text

--- app/generator/page.tsx
+++ app/generator/page.tsx
@@ -1,27 +1,73 @@
+'use client'
+
 import { getRecommendationsData } from '../actions'
 import TrackCard from '@/components/shared/TrackCard'
+import { Button } from '@/components/ui/button'
 import { SpotifyTrack } from '@/lib/types'
-
-export const dynamic = 'force-dynamic'
-
-const tracks = ['2IRZnDFmlqMuOrYOLnZZyc']
+import { useRouter } from 'next/navigation'
 
 export default async function Generator() {
-    const results: SpotifyTrack[] = await getRecommendationsData(tracks)
+    const router = useRouter()
 
     return (
-        <section className="space-y-2 p-12">
-            Generator
-            <div className="inset-4 flex min-h-60 w-full flex-row flex-wrap justify-center gap-2 bg-slate-600 p-4">
-                {/* {results?.map((track, idx) => {
-                    return (
-                        <TrackCard
-                            key={'search' + track.spotifyId}
-                            track={track}
-                            variant={'search'}
-                        />
-                    )
-                })} */}
+        <section className="h-full p-12">
+            <h3 className="text-center text-2xl underline my-4">HOW IT WORKS</h3>
+
+            <div className="grid h-full w-full grid-cols-1 md:grid-cols-3 gap-16 rounded-lg bg-slate-600/40 p-12 text-lg">
+                <div className="col-span-1 flex flex-col gap-8">
+                    <h4 className="text-center text-xl">
+                        <span className="font-bold text-indigo-300">Step 1: </span>
+                        Search
+                    </h4>
+
+                    <div className="border-x border-emerald-500 p-4">
+                        <p>
+                            To begin, search for any songs you can find on
+                            Spotify. You can select up to five songs, and the
+                            recommendations will be based on the overall
+                            qualities present in these selections.
+                        </p>
+                    </div>
+                </div>
+                <div className="col-span-1 flex flex-col gap-8">
+                    <h4 className="text-center text-xl">
+                        <span className="font-bold text-indigo-300">Step 2: </span>
+                        Adjustments
+                    </h4>
+
+                    <div className="border-x border-emerald-500 p-4">
+                        <p>
+                            Here, you'll find different qualities of the songs
+                            you can tweak to adjust your results. Each of these
+                            adjustments are optional, and represent musical
+                            features within a song.
+                        </p>
+                    </div>
+                </div>
+                <div className="col-span-1 flex flex-col gap-8">
+                    <h4 className="text-center text-xl">
+                        <span className="font-bold text-indigo-300">Step 3: </span>
+                        Results
+                    </h4>
+                    <div className="border-x border-emerald-500 p-4">
+                        <p>
+                            Your selected songs and any adjustments you make
+                            will retreive recommended tracks from Spotify. Follow
+                            the link to listen or save these songs to your
+                            personal library.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="mt-8 flex flex-row justify-center">
+                <Button
+                    onClick={() => router.push('/generator/g/search')}
+                    size={'xl'}
+                    className="text-2xl"
+                    variant={'app1'}
+                >
+                    Begin
+                </Button>
             </div>
         </section>
     )
