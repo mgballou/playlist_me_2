6389f1c3c7146332aca8009c21c6d0de8f20b7de
mgballou <matthewbuiltthat@gmail.com>
2024-07-05 23:29:49 -0400

    typescript problems

--- app/actions.ts
+++ app/actions.ts
@@ -29,9 +29,8 @@ export async function getSearchResults(query: string) {
         const response = await fetch(url + '?' + params.toString(), options)
 
         if (!response.ok) {
-            const errMessage = (await response.json()).message
-            console.log(errMessage)
-            throw new Error(errMessage)
+            console.log(await response.json())
+            throw new Error(await response.json())
         }
 
         const responseData = await response.json()
