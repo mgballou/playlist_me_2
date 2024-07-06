e59ba4fd9d49b57d040ac67d556ebea5c0ba4ef7
mgballou <matthewbuiltthat@gmail.com>
2024-07-05 23:02:34 -0400

    error adjustment

--- app/actions.ts
+++ app/actions.ts
@@ -29,8 +29,8 @@ export async function getSearchResults(query: string) {
         const response = await fetch(url + '?' + params.toString(), options)
 
         if (!response.ok) {
-            console.log(await response.json())
-            throw new Error(await response.json())
+            
+            throw new Error((await response.json()).error)
         }
 
         const responseData = await response.json()
