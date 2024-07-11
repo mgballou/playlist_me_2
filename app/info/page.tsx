d4e7a9cb5d0a808d7858076d9b9901eb136ca8a6
mgballou <matthewbuiltthat@gmail.com>
2024-07-10 23:29:25 -0400

    build error

--- app/info/page.tsx
+++ app/info/page.tsx
@@ -38,9 +38,9 @@ function Purpose() {
                 <p>
                     I wanted a way to rapidly discover new tracks that matched
                     the vibe of ones I had already selected for a certain
-                    purpose. But, if those songs didn't necessarily match my
-                    everyday taste in music, Spotify's recommendation engine
-                    left a lot to be desired.
+                    purpose. But, if those songs didn&apos;t necessarily match
+                    my everyday taste in music, Spotify&apos;s recommendation
+                    engine left a lot to be desired.
                 </p>
             </CardContent>
         </Card>
@@ -59,26 +59,26 @@ function Project() {
             </CardHeader>
             <CardContent className="space-y-3 md:space-y-5">
                 <p>
-                    Across my recent personal projects, I've been searching for
-                    a set of technologies that I felt excited to use every time.
-                    For Playlist.me, I hoped to settle into a tech stack that
-                    I'd feel comfortable continuing to use for my next several
-                    projects.
+                    Across my recent personal projects, I&apos;ve been searching
+                    for a set of technologies that I felt excited to use every
+                    time. For Playlist.me, I hoped to settle into a tech stack
+                    that I&apos;d feel comfortable continuing to use for my next
+                    several projects.
                 </p>
                 <p>
                     React has been my preference for some time, but before this,
-                    hadn't yet dived into Next.js, and saw this as an excellent
-                    opportunity. Adapting to the tight coupling of server and
-                    client while still working in a paradigm I mesh with was the
-                    right blend of comfort and challenge.
+                    hadn&apos;t yet dived into Next.js, and saw this as an
+                    excellent opportunity. Adapting to the tight coupling of
+                    server and client while still working in a paradigm I mesh
+                    with was the right blend of comfort and challenge.
                 </p>
                 <p>
-                    In the past I've worked with a variety of CSS solutions and
-                    component libraries. The utility-first approach of Tailwind
-                    is something I love, but I also enjoy libraries like Chakra
-                    UI for well-designed components. When i discovered
-                    shadcn/ui, built on top of Radix and Tailwind, I was
-                    immediately sold.
+                    In the past I&apos;ve worked with a variety of CSS solutions
+                    and component libraries. The utility-first approach of
+                    Tailwind is something I love, but I also enjoy libraries
+                    like Chakra UI for well-designed components. When I
+                    discovered shadcn/ui, built on top of Radix and Tailwind, I
+                    was immediately sold.
                 </p>
                 <p></p>
             </CardContent>
@@ -107,19 +107,19 @@ function Process() {
                     polished, or impressive.
                 </p>
                 <p>
-                    I couldn't say that I was truly happy with what I had put
-                    together, but at the same time I was proud of what was my
-                    first ever functional web application. I moved on to other
-                    projects, but could never shake a nagging feeling that
+                    I couldn&apos;t say that I was truly happy with what I had
+                    put together, but at the same time I was proud of what was
+                    my first ever functional web application. I moved on to
+                    other projects, but could never shake a nagging feeling that
                     Playlist.me could grow to be more than what it initially
                     was.
                 </p>
                 <p>
                     Even now, I still see lots of ways that this application
-                    could be improved. I'll consider it to be in active
+                    could be improved. I&apos;ll consider it to be in active
                     development for some time yet, because this project means a
                     lot to me. That being said, the differences between v1 and
-                    v2 are evidence of just how much I've progressed as an
+                    v2 are evidence of just how much I&apos;ve progressed as an
                     engineer.
                 </p>
             </CardContent>
