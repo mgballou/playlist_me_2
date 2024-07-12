29db3c194f6cb06c0bc75ca506904f5078aefb6c
mgballou <matthewbuiltthat@gmail.com>
2024-07-11 20:28:53 -0400

    fetchCache force-no-store

--- app/actions.ts
+++ app/actions.ts
@@ -2,6 +2,8 @@
 
 import { Feature, SpotifyTrack } from '@/lib/types'
 
+export const fetchCache = 'force-no-store'
+
 export async function getAPIToken() {
     const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
     const url = `${baseUrl}api/spotify/auth`
--- app/api/spotify/auth/route.ts
+++ app/api/spotify/auth/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-export const dynamic = 'force-dynamic'
+export const fetchCache = 'force-no-store'
 
 export async function GET() {
     console.log(
